8e941233f6f822b48280df0762480524419fbaba
red-ornob <red.ornob.dev@gmail.com>
2025-06-23 14:28:14 +0600

    configured the argparse

--- pom.py
+++ pom.py
@@ -1,10 +1,21 @@
-import chime
+import argparse
 from datetime import datetime as dt
 import sys
 import time
 
+try:
+    import chime
+except ImportError:
+    print("chime module not installed.\n" \
+          "Install it with pip, preferably inside a virtual env")
+    sys.exit(1)
 
-def pom(work_time: int, break_time: int, pom_count: int) -> None:
+
+def main():
+    pom(*args())
+
+
+def pom(pom_count: int, work_time: int, break_time: int) -> None:
     print(f"Starting the a session of {pom_count} {work_time}/{break_time}poms. {dt.now().strftime("%H:%M")}")
     for i in range(pom_count):
         if i > 1: print(f"Starting pom #{i}, get back to work! {dt.now().strftime("%H:%M")}")
@@ -15,9 +26,14 @@ def pom(work_time: int, break_time: int, pom_count: int) -> None:
         chime.success()
     print(f"Completed all {pom_count} poms, good work! {dt.now().strftime("%H:%M")}")
 
-def main():
-    if len(sys.argv) == 4:
-        pom(*[int(i) for i in sys.argv[1:]])
+
+def args() -> tuple[int, int, int]:
+    parser = argparse.ArgumentParser(prog='python pom.py', description='CLI Pomodoro Timer written in Python')
+    parser.add_argument('pom_count', type=int, help='Number of poms')
+    parser.add_argument('work_time', type=int, help='Work duration in minutes')
+    parser.add_argument('break_time', type=int, help='Break duration in minutes')
+    args = parser.parse_args()
+    return (args.pom_count, args.work_time, args.break_time)
 
 
 if __name__ == "__main__":
